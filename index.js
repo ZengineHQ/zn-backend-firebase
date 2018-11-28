d3b8e8ffc0dc46e88b15d584ccff8ca5b299006f
tehpsalmist <bstewardcodes@gmail.com>
2018-11-27 19:33:17 -0500

    feat: transaction

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var znFirebase = require('../../../lib/zn-firebase');
+var znFirebase = require('../zn-plugin-sreb-number-assignment/backend/assign-number/lib/zn-firebase');
 var Q = require('q');
 
 /**
@@ -35,7 +35,7 @@ module.exports.expandPath = function (path) {
 module.exports.load = function (path) {
 	var def = Q.defer();
 
-	module.exports.expandPath(path).once('value', function(snapshot) {
+	module.exports.expandPath(path).once('value', function (snapshot) {
 		def.resolve(snapshot.val());
 	}, function (err) {
 		def.reject(err);
@@ -68,3 +68,30 @@ module.exports.save = function (path, data) {
 
 	return def.promise;
 };
+
+/**
+ * Helper to save arbitrary data to Firebase.
+ *
+ * @param {Array<string>} path An array of path components that will be concatenated with '/' as the separator.
+ * 	Ex: ['foo', 'bar', 'baz'] will become 'foo/bar/baz'
+ * @param {function(string|number|boolean|null): string|number|boolean|undefined} updateFunction
+ * @param {boolean} applyLocally Will determine whether or not to return events from the
+ *
+ * @return {Promise}
+ *
+ * @see expandFirebasePath
+ */
+module.exports.transaction = function (path, updateFunction, applyLocally) {
+	var def = Q.defer();
+	var ref = expandFirebasePath(path);
+
+	ref.transaction(updateFunction, function (err, committed, snapshot) {
+		if (err || !committed) {
+			def.reject(err || 'update function return undefined; value not saved')
+		} else {
+			def.resolve(snapshot.val())
+		}
+	}, applyLocally);
+
+	return def.promise;
+}
